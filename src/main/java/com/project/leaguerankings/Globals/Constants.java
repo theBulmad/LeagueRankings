9f680f8f7791aeb207a4b2a05b460322fd100f4e
Bulumko <thebulmad@gmail.com>
2022-02-18 17:30:17 +0200

    Minor fixes

--- src/main/java/com/project/leaguerankings/Globals/Constants.java
+++ src/main/java/com/project/leaguerankings/Globals/Constants.java
@@ -1,16 +1,13 @@
 package com.project.leaguerankings.Globals;
 
-public class Constants {
+public interface Constants {
 
-    private Constants(){
-        throw new IllegalStateException("Constants Class");
-    }
+    String QUIT_COMMAND = "Q";
+    String YES_COMMAND = "Y";
+    String START_COMMAND = "S";
+    String DONE_COMMAND = "D";
+    int WIN = 3;
+    int DRAW = 1;
+    String CLUBNAME_MULTIPLE_WHITESPACE_REGEX = "[a-zA-Z]+\\s+[a-zA-Z]+ \\d"; //FC Awesome 3 - more than 1 space when split by whitespace
 
-    public static final String QUIT_COMMAND = "Q";
-    public static final String YES_COMMAND = "Y";
-    public static final String START_COMMAND = "S";
-    public static final String DONE_COMMAND = "D";
-    public static final int WIN = 3;
-    public static final int DRAW = 1;
-    public static final String CLUBNAME_MULTIPLE_WHITESPACE_REGEX = "[a-zA-Z]+\\s+[a-zA-Z]+ \\d"; //FC Awesome 3 - more than 1 space when split by whitespace
 }
